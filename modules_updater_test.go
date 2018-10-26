216f49b2c717837ca390498dbac350419801ccf2
y-yagi <y-yagi@users.noreply.github.com>
2018-10-26 16:14:41 +0900

    add test for `generatePullRequestBody`

---
+++ modules_updater_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestGeneratePullRequestBody(t *testing.T) {
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	updater := NewModulesUpdater(nil)
+	before, err := updater.readModules(filepath.Join(wd, "testdata", "before"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	after, err := updater.readModules(filepath.Join(wd, "testdata", "after"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	got := updater.generatePullRequestBody(before, after)
+	want := `**Changed:**
+
+* [github.com/go-pg/pg](https://github.com/go-pg/pg) [v6.14.0...v6.15.1](https://github.com/go-pg/pg/compare/v6.14.0...v6.15.1)
+* [golang.org/x/crypto](https://github.com/golang/crypto) [74cb1d3d52f4...e84da0312774](https://github.com/golang/crypto/compare/74cb1d3d52f4...e84da0312774)
+* [golang.org/x/sys](https://github.com/golang/sys) [5cd93ef61a7c...d989b31c8746](https://github.com/golang/sys/compare/5cd93ef61a7c...d989b31c8746)
+`
+
+	if got != want {
+		t.Fatalf("want\n%v\nbut got \n\n%v", want, got)
+	}
+}
