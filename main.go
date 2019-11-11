110878a4101ae37380b7d183d8c48473bfed926d
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2019-11-12 08:54:03 +0900

    Bump version

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func run(args []string, outStream, errStream io.Writer) int {
 	app := cli.NewApp()
 	app.Name = "ci-modules-update"
 	app.Usage = "create a modules update PR"
-	app.Version = "0.1.2"
+	app.Version = "0.1.3"
 	app.Flags = commandFlags()
 	app.Action = appRun
 
